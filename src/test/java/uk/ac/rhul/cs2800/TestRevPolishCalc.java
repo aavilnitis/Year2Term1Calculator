f148ccd68be04a49f635be50a8b74ab5d4493e2c
zkac401 <zkac401@live.rhul.ac.uk>
2022-11-25 14:48:39 +0000

    Refactoring TestrevPolishCalc.java once again: As the previous test cases were only temporary to see if the other methods are layout of the evaluate() works, I had to remove them to continue working on the rest of the method and start actually adding functionality

--- src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
+++ src/test/java/uk/ac/rhul/cs2800/TestRevPolishCalc.java
@@ -18,21 +18,12 @@ class TestRevPolishCalc {
 		assertEquals(revPolishCalc.isOperator("+"), true, "The isOperator method should return true with input +");
 		assertEquals(revPolishCalc.isOperator("G"), false, "The isOperator method should return false with input G");
 	}
-
 	
 	@Test
 	void testIsNumeric() {
 		assertEquals(revPolishCalc.isNumeric("12"), true, "The isNumeric method should return true with input 12");
 		assertEquals(revPolishCalc.isNumeric("bbb"), false, "The isNumeric method should return false with input bbb");
 
-	}
-	
-	//Evaluate testing: Returns 0f = operator, 1f = numeric, 3f = invalid.
-	@Test
-	void testEvaluateOptions() {
-		assertEquals(revPolishCalc.evaluate("1"), 1f, "The evaluate method should return 1f if I pass in a numeric value.");
-		assertEquals(revPolishCalc.evaluate("+"), 0f, "The evaluate method should return 0f if I pass in an operator value.");
-		assertEquals(revPolishCalc.evaluate("."), 2f, "The evaluate method should return 2f if I pass in an invalid value.");
 	}
 
 }
